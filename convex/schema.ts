bc6745edffb50e5d57e2f88f172f6793b725fcf4
Amr Khaled <lewolfje@gmail.com>
2024-08-06 18:17:58 +0300

    add friends, conversations, conversationMembers and messages tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -15,4 +15,31 @@ export default defineSchema({
   })
     .index("by_receiver", ["receiver"])
     .index("by_receiver_sender", ["receiver", "sender"]),
+  friends: defineTable({
+    user1: v.id("users"),
+    user2: v.id("users"),
+    conversationId: v.id("conversations"),
+  })
+    .index("by_user1", ["user1"])
+    .index("by_user2", ["user2"])
+    .index("by_conversationId", ["conversationId"]),
+  conversations: defineTable({
+    name: v.optional(v.string()),
+    isGroup: v.boolean(),
+    lastMessageId: v.optional(v.id("messages")),
+  }),
+  conversationMembers: defineTable({
+    memberId: v.id("users"),
+    conversationId: v.id("conversations"),
+    lastSeenMessageId: v.optional(v.id("messages")),
+  })
+    .index("by_memberId", ["memberId"])
+    .index("by_conversationId", ["conversationId"])
+    .index("by_conversationId_memberId", ["conversationId", "memberId"]),
+  messages: defineTable({
+    sender: v.id("users"),
+    conversationId: v.id("conversations"),
+    type: v.string(),
+    content: v.array(v.string()),
+  }).index("by_conversationId", ["conversationId"]),
 });
