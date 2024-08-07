ee5697eaec88482900a73405f61920c5685f6dfb
Amr Khaled <lewolfje@gmail.com>
2024-08-07 19:37:17 +0300

    convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
 import type * as request from "../request.js";
 import type * as user from "../user.js";
@@ -28,6 +29,7 @@ import type * as _utils from "../_utils.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  conversations: typeof conversations;
   http: typeof http;
   request: typeof request;
   user: typeof user;
