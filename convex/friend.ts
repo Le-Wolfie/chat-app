37bed0acba55a2008f6efb13e8be33aafaf6a1c7
Amr Khaled <lewolfje@gmail.com>
2024-08-10 19:42:41 +0300

    mutation to remove a friend

---
+++ convex/friend.ts
@@ -0,0 +1,69 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { getUserByClerkId } from "./_utils";
+
+export const remove = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError("Conversation not found");
+    }
+
+    const memberships = await ctx.db
+      .query("conversationMembers")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    if (!memberships || memberships.length !== 2) {
+      throw new ConvexError("This conversation does not have any members");
+    }
+
+    const friendship = await ctx.db
+      .query("friends")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .unique();
+
+    if (!friendship) {
+      throw new ConvexError("Not a friend");
+    }
+
+    const messages = await ctx.db
+      .query("messages")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    await ctx.db.delete(args.conversationId);
+    await ctx.db.delete(friendship._id);
+
+    await Promise.all(
+      memberships.map((membership) => ctx.db.delete(membership._id))
+    );
+
+    await Promise.all(messages.map((message) => ctx.db.delete(message._id)));
+  },
+});
