0762208769b4d10ef23087a4d2463cafeb1963ea
Amr Khaled <lewolfje@gmail.com>
2024-08-12 13:47:59 +0300

    add leave and delete group functions

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getUserByClerkId } from "./_utils";
 import { QueryCtx, MutationCtx } from "./_generated/server";
 import { Id } from "./_generated/dataModel";
@@ -200,3 +200,98 @@ const getMessageContent = (type: string, content: string) => {
       return "[non-text]";
   }
 };
+
+export const deleteGroup = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError("Conversation not found");
+    }
+
+    const memberships = await ctx.db
+      .query("conversationMembers")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    if (!memberships || memberships.length <= 1) {
+      throw new ConvexError("This conversation does not have any members");
+    }
+
+    const messages = await ctx.db
+      .query("messages")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    await ctx.db.delete(args.conversationId);
+
+    await Promise.all(
+      memberships.map((membership) => ctx.db.delete(membership._id))
+    );
+
+    await Promise.all(messages.map((message) => ctx.db.delete(message._id)));
+  },
+});
+
+export const leaveGroup = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError("Conversation not found");
+    }
+
+    const membership = await ctx.db
+      .query("conversationMembers")
+      .withIndex("by_memberId_conversationId", (q) =>
+        q.eq("memberId", currentUser._id).eq("conversationId", conversation._id)
+      )
+      .unique();
+
+    if (!membership) {
+      throw new ConvexError("You are not a member of this group");
+    }
+
+    await ctx.db.delete(membership._id);
+  },
+});
