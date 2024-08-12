467c4c2cc29e82744302628eff40aa9a2fa05f8e
Amr Khaled <lewolfje@gmail.com>
2024-08-12 12:47:06 +0300

    add group chat query and mutation

---
+++ convex/friends.ts
@@ -0,0 +1,90 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getUserByClerkId } from "./_utils";
+
+export const conversation = query({
+  args: {
+    id: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const friendships1 = await ctx.db
+      .query("friends")
+      .withIndex("by_user1", (q) => q.eq("user1", currentUser._id))
+      .collect();
+
+    const friendships2 = await ctx.db
+      .query("friends")
+      .withIndex("by_user2", (q) => q.eq("user2", currentUser._id))
+      .collect();
+
+    const friendships = [...friendships1, ...friendships2];
+
+    const friends = await Promise.all(
+      friendships.map(async (friendship) => {
+        const friend = await ctx.db.get(
+          currentUser._id === friendship.user1
+            ? friendship.user2
+            : friendship.user1
+        );
+
+        if (!friend) {
+          throw new ConvexError("Friend not found");
+        }
+
+        return friend;
+      })
+    );
+  },
+});
+
+export const createGroup = mutation({
+  args: {
+    members: v.array(v.id("users")),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversationId = await ctx.db.insert("conversations", {
+      isGroup: true,
+      name: args.name,
+    });
+
+    await Promise.all(
+      [...args.members, currentUser._id].map((memberId) =>
+        ctx.db.insert("conversationMembers", {
+          conversationId,
+          memberId,
+        })
+      )
+    );
+  },
+});
